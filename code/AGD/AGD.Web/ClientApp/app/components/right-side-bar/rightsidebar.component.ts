b894f907d370e69d57b52539860556d69d068227
thuanhd <thuan@htactive.com>
2017-12-04 15:52:40 +0700

    left side bar

---
+++ code/AGD/AGD.Web/ClientApp/app/components/right-side-bar/rightsidebar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'right-side-bar',
+    templateUrl: './rightsidebar.component.html',
+})
+export class RightSideBarComponent {
+}
